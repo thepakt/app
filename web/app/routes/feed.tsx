9851af5e60c7b34b0559c30a72b6022522f703a3
Kisuyo <ig.intr3st@gmail.com>
2024-10-18 16:41:59 +0200

    fix:dark theme feed

--- web/app/routes/feed.tsx
+++ web/app/routes/feed.tsx
@@ -71,22 +71,22 @@ function RouteComponent() {
           {feedItems.map((item) => (
             <li
               key={item.id}
-              className="flex flex-col bg-white p-4 rounded-lg drop-shadow-md gap-2"
+              className="flex flex-col bg-neutral-800 p-4 rounded-lg drop-shadow-md gap-2"
             >
               <article>
                 <header className="flex items-center justify-between gap-2">
                   <div className="flex items-center gap-2">
-                    <img className="min-w-[42px] w-[42px] h-[42px] bg-gray-200/60 rounded-full" />
+                    <div className="min-w-[42px] w-[42px] h-[42px] bg-neutral-700 rounded-full" />
                     <div className="flex flex-col">
                       <h2 className="text-sm font-bold ">{item.prettyName}</h2>
                       <p className="text-xs text-gray-500">{item.username}</p>
                     </div>
                   </div>
-                  <span className="w-fit text-end text-[14px] font-medium bg-gray-200/60 px-3 rounded-md p-1">
+                  <span className="w-fit text-end text-[14px] font-medium bg-neutral-700 px-3 rounded-md p-1">
                     {item.bounty}$
                   </span>
                 </header>
-                <p className="mt-2 p-2 px-3 text-[14px] max-h-[160px] overflow-auto no-scrollbar bg-gray-200/60 rounded-lg">
+                <p className="mt-2 p-2 px-3 text-[14px] max-h-[160px] overflow-auto no-scrollbar bg-neutral-700/60 rounded-lg">
                   {item.description}
                 </p>
                 <ul className="flex flex-col gap-2 mt-2">
@@ -95,10 +95,10 @@ function RouteComponent() {
                       key={subtask.id}
                       className="flex items-center gap-3 w-full"
                     >
-                      <span className="w-2 h-2 bg-gray-400 rounded-full"></span>
-                      <p className="text-[14px] flex items-center justify-between p-2 bg-gray-200/60 rounded-lg w-full">
+                      <span className="w-2 h-2 bg-gray-600 rounded-full"></span>
+                      <p className="text-[14px] flex items-center justify-between p-2 bg-neutral-700/60 rounded-lg w-full">
                         {subtask.title}
-                        <span className="text-[12px] text-gray-500 font-medium">
+                        <span className="text-[12px] text-gray-400 pr-1 font-medium">
                           {subtask.bounty}$
                         </span>
                       </p>

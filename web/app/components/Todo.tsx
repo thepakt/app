e07125050979a016f3082c4ec840f8f85bd9aebc
marshennikovaolga <marshennikova@gmail.com>
2024-10-18 14:50:44 +0200

    .

--- web/app/components/Todo.tsx
+++ web/app/components/Todo.tsx
@@ -1,7 +1,6 @@
 import { motion, AnimatePresence } from "framer-motion"
-import { SquareIcon } from "lucide-react"
+import { CheckCircle2, Circle } from "lucide-react"
 import { useEffect, useState } from "react"
-import { modalSpring } from "theme/transitions"
 
 interface TodoProps {
   id: number
@@ -30,28 +29,24 @@ const Todo: React.FC<TodoProps> = ({
     <motion.div
       layout
       onClick={onClick}
-      className={`todo-item p-2 pl-3 rounded-lg text-neutral-700 font-medium cursor-pointer overflow-hidden ${
-        isExpanded ? "bg-white" : "bg-transparent"
+      className={`todo-item p-4 rounded-xl text-white font-medium cursor-pointer overflow-hidden ${
+        isExpanded ? "bg-white/10 backdrop-blur-md" : "bg-white/5"
       }`}
       initial={false}
       animate={{
-        height: isExpanded ? "200px" : "40px",
-        scale: isExpanded ? 1.02 : 1,
-      }}
-      transition={{
-        type: "spring",
-        stiffness: 400,
-        damping: 40,
-        mass: 1,
+        height: isExpanded ? "auto" : "60px",
+        transition: {
+          type: "spring",
+          stiffness: 300,
+          damping: 30,
+        },
       }}
     >
       <div className="flex flex-col h-full">
-        <motion.div
-          className="flex gap-1 items-center pb-0.5 h-[36px] flex-shrink-0"
-          layout="position"
-        >
-          <SquareIcon size={18} />
-          <h3 className="truncate pb-0.5">{title}</h3>
+        <motion.div className="flex items-center gap-3" layout="position">
+          <Circle className="text-gray-400" size={24} />
+
+          <h3 className="text-lg font-semibold truncate">{title}</h3>
         </motion.div>
         <AnimatePresence>
           {isExpanded && (
@@ -59,68 +54,44 @@ const Todo: React.FC<TodoProps> = ({
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
               exit={{ opacity: 0, y: -20 }}
-              transition={{
-                type: "spring",
-                stiffness: 300,
-                damping: 25,
-                delay: 0.3,
-              }}
-              className="flex flex-col flex-grow justify-between items-between text-[14px] font-normal gap-2 p-2"
+              transition={{ delay: 0.1 }}
+              className="flex flex-col mt-4 text-sm"
             >
-              <p className="">Something something...</p>
-              <motion.div
-                className="bg-primary-500 h-[30px] items-center max-w-full w-fit flex gap-1 self-end rounded-md overflow-hidden"
-                animate={{
-                  width: isBountyExpanded
-                    ? "300px"
-                    : bounty
-                      ? "fit-content"
-                      : "120px",
-                }}
-              >
-                <AnimatePresence>
-                  {!isBountyExpanded && (
-                    <motion.div
-                      className="bg-primary-500 uppercase font-semibold min-w-fit flex items-center justify-center text-white rounded-md px-3 p-1 text-[12px]"
-                      exit={{ opacity: 0 }}
-                      initial={{ opacity: 1 }}
-                      transition={{ duration: 0.3 }}
+              <p className="opacity-50 mb-4">Something something...</p>
+              <div className="flex justify-between items-center">
+                <span className="text-gray-400">Bounty:</span>
+                <motion.div
+                  className="bg-white/20 rounded-full overflow-hidden"
+                  animate={{
+                    width: isBountyExpanded ? "100px" : "auto",
+                  }}
+                >
+                  {isBountyExpanded ? (
+                    <div className="flex flex-row gap-1 items-center w-full bg-transparent pr-3 py-1">
+                      {/* <span className="text-gray-400">$</span> */}
+                      <input
+                        type="number"
+                        className="w-full bg-transparent text-right outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none pl-0"
+                        defaultValue={bounty}
+                        onClick={(e) => {
+                          e.stopPropagation()
+                          setIsBountyExpanded(true)
+                        }}
+                      />
+                    </div>
+                  ) : (
+                    <span
+                      className="px-3 py-1 block"
                       onClick={(e) => {
                         e.stopPropagation()
                         setIsBountyExpanded(true)
                       }}
                     >
-                      {bounty ? `${bounty}$` : "set bounty"}
-                    </motion.div>
+                      ${bounty}
+                    </span>
                   )}
-                </AnimatePresence>
-                {isBountyExpanded && (
-                  <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ duration: 0.3, delay: 0.3 }}
-                    className=" flex items-center justify-between w-full h-full p-0.5 pl-3"
-                  >
-                    {" "}
-                    <input
-                      type="number"
-                      autoFocus
-                      className="w-full placeholder-white outline-none h-full text-[12px] bg-transparent text-white rounded"
-                      placeholder="Enter bounty amount"
-                    />
-                    <button
-                      onClick={(e) => {
-                        e.stopPropagation()
-                        setIsBountyExpanded(false)
-                      }}
-                      className="bg-white text-black font-medium rounded-md px-4 py-1 text-[12px]"
-                    >
-                      SET
-                    </button>
-                  </motion.div>
-                )}
-              </motion.div>
+                </motion.div>
+              </div>
             </motion.div>
           )}
         </AnimatePresence>

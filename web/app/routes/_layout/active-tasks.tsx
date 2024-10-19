740d9636c1863c797f58ef615eb2b8c9966528c3
Nikita <github@nikiv.dev>
2024-10-19 17:07:23 +0200

    .

--- web/app/routes/_layout/active-tasks.tsx
+++ web/app/routes/_layout/active-tasks.tsx
@@ -5,12 +5,26 @@ import { AnimatePresence } from "framer-motion"
 import { useEffect, useState } from "react"
 import { getActiveTasks } from "~/actions"
 import AddTodoButton from "~/components/AddTodoButton"
-import Layout from "~/components/Layout"
 import NewTodo from "~/components/NewTodo"
 import { TaskComponent } from "~/components/TaskComponent"
 
-function TasksComponent() {
+function RouteComponent() {
   const address = useTonAddress()
+  const [isNewTodoOpen, setIsNewTodoOpen] = useState(false)
+  const [expandedTodoId, setExpandedTodoId] = useState<number | null>(null)
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as HTMLElement
+      if (!target.closest(".todo-item")) {
+        setExpandedTodoId(null)
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
+
   const { data, isLoading, error } = useQuery({
     queryKey: ["/active-tasks"],
     queryFn: async () => {
@@ -25,55 +39,42 @@ function TasksComponent() {
     return <></>
   }
 
-  const [isNewTodoOpen, setIsNewTodoOpen] = useState(false)
-  const [expandedTodoId, setExpandedTodoId] = useState<number | null>(null)
-
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      const target = event.target as HTMLElement
-      if (!target.closest(".todo-item")) {
-        setExpandedTodoId(null)
-      }
-    }
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-  }, [])
   console.log(data, "data")
-  return <></>
-  // return (
-  //     <div className="w-full flex items-center justify-center">
-  //       <div className="w-full p-4 py-[0.5em] min-w-[300px] max-w-[500px] h-full">
-  //         <div className="flex flex-col gap-1 mt-16">
-  //           <AnimatePresence>
-  //             {isNewTodoOpen && (
-  //               <NewTodo onClose={() => setIsNewTodoOpen(false)} />
-  //             )}
-  //           </AnimatePresence>
-  //           {data?.tasks?.map((task) => (
-  //             <TaskComponent
-  //               key={task.id}
-  //               task={task}
-  //               // @ts-ignore
-  //               isExpanded={expandedTodoId === task.id}
-  //               onClick={(id) => {
-  //                 setExpandedTodoId((prevId) => (prevId === id ? null : id))
-  //               }}
-  //             />
-  //           ))}
-  //         </div>
-  //       </div>
-  //     </div>
-  //     <div className="absolute bottom-20 left-0 right-2 flex justify-center">
-  //       <AddTodoButton
-  //         setIsNewTodoOpen={setIsNewTodoOpen}
-  //         isNewTodoOpen={isNewTodoOpen}
-  //       />
-  //     </div>
-  // )
+
+  return (
+    <>
+      <div className="w-full flex items-center justify-center">
+        <div className="w-full p-4 py-[0.5em] min-w-[300px] max-w-[500px] h-full">
+          <div className="flex flex-col gap-1 mt-16">
+            <AnimatePresence>
+              {isNewTodoOpen && (
+                <NewTodo onClose={() => setIsNewTodoOpen(false)} />
+              )}
+            </AnimatePresence>
+            {/* {data?.map((task) => (
+              <TaskComponent
+                key={task.id}
+                task={task}
+                // @ts-ignore
+                isExpanded={expandedTodoId === task.id}
+                onClick={(id) => {
+                  setExpandedTodoId((prevId) => (prevId === id ? null : id))
+                }}
+              />
+            ))} */}
+          </div>
+        </div>
+      </div>
+      <div className="absolute bottom-20 left-0 right-2 flex justify-center">
+        <AddTodoButton
+          setIsNewTodoOpen={setIsNewTodoOpen}
+          isNewTodoOpen={isNewTodoOpen}
+        />
+      </div>
+    </>
+  )
 }
 
 export const Route = createFileRoute("/_layout/active-tasks")({
-  component: TasksComponent,
+  component: RouteComponent,
 })

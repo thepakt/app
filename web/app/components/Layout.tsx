04c46260b90fee4e2442f2bc348d2b0758649f2d
Kisuyo <ig.intr3st@gmail.com>
2024-10-19 17:51:12 +0200

    chore: profile ui

--- web/app/components/Layout.tsx
+++ web/app/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { TonConnectButton } from "@tonconnect/ui-react"
 import React from "react"
 import Bottombar from "./BottomBar"
+import { useRouter } from "@tanstack/react-router"
 
 interface LayoutProps {
   children: React.ReactNode
@@ -8,13 +9,18 @@ interface LayoutProps {
 
 // TODO: delete this..
 function Layout({ children }: LayoutProps) {
+  const pathname = useRouter().state.location.pathname
   return (
-    <div className="relative w-full h-screen">
-      <div className="absolute top-4 right-4">
-        <TonConnectButton />
+    <div className="relative w-full">
+      <div className="w-full relative max-w-2xl mx-auto">
+        {pathname !== "/profile" && (
+          <div className="absolute top-4 right-4">
+            <TonConnectButton />
+          </div>
+        )}
+        {children}
+        <Bottombar />
       </div>
-      {children}
-      <Bottombar />
     </div>
   )
 }

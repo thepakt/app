86a92b2c8c81c9485ce26eda00751f90ab1c535d
Nikita <github@nikiv.dev>
2024-10-20 12:58:57 +0200

    .

--- web/app.config.ts
+++ web/app.config.ts
@@ -17,10 +17,6 @@ export default defineConfig({
     },
   },
   vite: {
-    // @ts-ignore
-    build: {
-      target: "es2022",
-    },
     plugins: () => [
       tsConfigPaths({
         projects: ["./tsconfig.json"],

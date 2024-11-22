0ff76d48bb1b830f0f1e3b45216fa793b940c25d
Evgeny Kniazev <eugene.kniazev@gmail.com>
2024-11-22 19:31:10 +0100

    feat: disconnect your wallet

--- web/app/components/WalletConnectButton.tsx
+++ web/app/components/WalletConnectButton.tsx
@@ -1,10 +1,20 @@
-import { useTonAddress, useTonConnectModal } from "@tonconnect/ui-react"
+import {
+  useTonAddress,
+  useTonConnectModal,
+  useTonConnectUI,
+  useTonWallet,
+} from "@tonconnect/ui-react"
 import { Button, Menu, buttonStyles } from "./ui"
 import { IconChevronDown, IconLogout } from "justd-icons"
 
 export default function WalletConnectButton() {
   const { state, open, close } = useTonConnectModal()
   const address = useTonAddress(true)
+  const tonConnect = useTonConnectUI()
+
+  async function disconnect() {
+    await tonConnect[0].disconnect()
+  }
 
   return (
     <>
@@ -24,7 +34,11 @@ export default function WalletConnectButton() {
           <Menu.Content showArrow placement="bottom" className="min-w-48">
             <Menu.Item className="cursor-pointer">Copy address</Menu.Item>
             <Menu.Separator />
-            <Menu.Item isDanger className="cursor-pointer">
+            <Menu.Item
+              isDanger
+              className="cursor-pointer"
+              onAction={disconnect}
+            >
               <IconLogout />
               Disconnect
             </Menu.Item>

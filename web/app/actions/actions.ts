d385be61416e5ca34105fd9fec3a418b7f467604
Nikita <github@nikiv.dev>
2024-11-24 23:50:45 +0100

    move to older ronin ver.

--- web/app/actions/actions.ts
+++ web/app/actions/actions.ts
@@ -1,19 +1,53 @@
 import { createServerFn } from "@tanstack/start"
+import { get } from "ronin"
+import { zodValidator } from "@tanstack/zod-adapter"
+import { z } from "zod"
 
-export const $createTask = createServerFn({ method: "POST" }).handler(
-  async () => {
-    return { message: "test" }
-  },
-)
+// TODO: have to to do ts-ignore due to latest version of server fn is strict with JSON serialization and RONIN does something weird
+// https://discord.com/channels/719702312431386674/1309975248929685656
 
-export const $deleteTask = createServerFn({ method: "POST" }).handler(
-  async () => {
-    return { message: "test" }
-  },
-)
-
-export const $getUserByWalletAddress = createServerFn({
+export const $getActiveTasksAndNotifications = createServerFn({
   method: "POST",
-}).handler(async () => {
-  return { message: "test" }
 })
+  .validator(zodValidator(z.object({ walletAddress: z.string() })))
+  // @ts-ignore
+  .handler(async ({ data }) => {
+    const { walletAddress } = data
+    const user = await get.user.with({
+      walletAddress: data.walletAddress,
+    })
+    if (!user) throw new Error("User not found")
+    const tasks = await get.tasks.with({
+      creator: user,
+    })
+    const taskNotifications = await get.acceptTaskNotifications({
+      including: ["task", "reciever"],
+    })
+    return {
+      tasks,
+      taskNotifications,
+    }
+  })
+
+// export const $getActiveTasksAndNotifications = createServerFn({
+//   method: "POST",
+// }).handler(async (data: { walletAddress: string }) => {
+//     const user = await get.user.with({
+//       walletAddress: data.walletAddress,
+//     })
+//     if (!user) throw new Error("User not found")
+//     const tasks = await get.tasks.with({
+//       creator: user,
+//     })
+//     // const taskNotifications = await get.acceptTaskNotifications.with({
+//     //   reciever: user.id,
+//     // })
+//     const taskNotifications = await get.acceptTaskNotifications({
+//       including: ["task", "reciever"],
+//     })
+//     return {
+//       tasks,
+//       taskNotifications,
+//     }
+//       },
+// )
